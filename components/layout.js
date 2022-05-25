046f2f143142cb484084d68fa94ce5d74f366986
boluwatifeBE <boluwatifeemmanuel.be@gmail.com>
2022-05-25 12:13:25 +0100

    first commit

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function Layout({ children }) {
+	return (
+		<div className='flex flex-col min-h-screen'>
+			<header className='bg-gray-900 mb-8 py-4 text-white'>
+				<div className='container mx-auto flex justify-center'>
+					<Link href='/'>
+						<a>🏡Home</a>
+					</Link>
+					<span className='mx-auto'>Welcome to my blog</span>{' '}
+				</div>
+			</header>
+			<main className='container mx-auto flex-1'>{children}</main>
+			<footer className='bg-gray-900 mt-8 py-4 text-white'>
+				<div className='container mx-auto flex justify-center'>
+					&copy; 2022 boluwatife_be
+				</div>
+			</footer>
+		</div>
+	);
+}
